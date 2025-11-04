fd114abea3d6d140cdfe7ed694043d639cf961d0
Maruf <maruf60463000@gmail.com>
2025-11-04 23:50:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,458 @@
+
+let my=""
+let sy=""
+
+
+function ba(){
+
+sy=`
+
+
+#p2{  width: 250px;
+      border: none;
+      margin: auto;
+      margin-top: 10px;
+      border-radius:50%;
+      cursor: pointer;
+      background-color: #8eb3a6;
+}
+.o1{
+  border: solid rgb(83, 68, 68) 1px;
+  width: 300px;
+  margin: auto;
+  padding: auto;
+  text-align: center;
+  background-color: rgba(0, 255, 255, 0.301);
+  border-radius: 15px;
+  margin-top: 2%;
+}
+
+
+  
+  
+  #p3{
+  width: 50px;}
+  #p6,#p4,#p5{  width: 35px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      background-color: #8eb3a6;
+  }.icon{
+  width: 50px;
+  background-color: #f1ecec85;
+  border-radius: 1%;
+}
+.d1{
+margin: 2%;
+padding: 10px;
+width: 40%;
+border-radius: 520px;
+max-width: 300px;
+min-width: 200px;
+}
+
+@media (max-width: 620px) {
+.d1 {
+    width: 80%;
+  }
+}
+
+#r6{
+  height: 200px;
+  width: 200px;
+  cursor: pointer;
+}
+#k8{
+  width:30px;
+  margin: auto;
+  margin-left: px;
+  padding: 0px;
+}
+#w1{
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  margin: 0px;
+  padding: 0px;
+}
+#h2{
+  margin: 10px;
+  padding-bottom:3px;
+  text-align: center;
+ background-color: #ebafde00;
+ border-radius: 5px;
+  font-family: 'Courier New', Courier, monospace;
+}
+#t12{
+  margin-top: 10px;
+  padding: 8px 16px;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: #1e693e;
+}
+#h2:hover {
+  transform: scale(1.08);  /* শুধু যেটাতে hover করবে সেটাই বড় হবে */
+}.d1{
+  font-size: x-large;
+  background-color: rgba(245, 239, 239, 0.473);
+  color: black;
+}
+
+ #m1 { 
+      transition: opacity 0.6s ease;  /* fade animation */
+      opacity: 1;
+      margin: auto;
+      padding: 20px;
+      text-align: center;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 20px;
+    }
+
+    .img {
+      margin-top: 20%;
+      width: 120px;
+      height: 1200px;
+      border-radius: 50%;
+      overflow: hidden;
+    }
+
+  
+
+    .i1 {
+      font-size: 20px;
+      font-family: Arial, Helvetica, sans-serif;
+      color: black;
+      text-align: center;
+      margin: 10px;
+      padding: 10px;
+      border: solid 1px rgb(53, 44, 44);
+      border-radius: 10px;
+      background-color: rgba(235, 132, 132, 0.77) ;
+      
+    }
+#p6{
+  width: 50px;
+  }
+  #e1,#p6{
+  background-color: #14b429ff;}
+  #p3,#p4,#p5{  width: 35px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      background-color: #8eb3a6;
+}
+
+.f1 {
+  height: 380px;
+  width: 250px;
+  background-color: rgba(158, 134, 158, 0.69);
+  border-radius: 15px;
+  box-shadow: 0 0 10px hsla(0, 63%, 93%, 0.20);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  transition: all 0.3s ease;  /* Smooth animation */
+}
+
+.f1:hover {
+  transform: scale(1.05);  /* শুধু যেটাতে hover করবে সেটাই বড় হবে */
+}
+.f1:active {
+  transform: scale(1.00);  /* শুধু যেটাতে active করবে সেটাই small হবে */
+}
+ 
+ `
+
+
+my=`  <section class="f1">
+    <img src="image.png"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="image.png"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="image.png"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="image.png"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+</section>
+<section class="f1">
+    <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="image.png"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>
+<section class="f1">
+    <img src="https://tse3.mm.bing.net/th/id/OIP.-AqMGSsxsF0z5AkPDO2pCQHaJ5?cb=12&w=474&h=395&c=7&p=0"  class="img">
+    <p class="i1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, rem ipsum a error sit voluptates mque adipisci blanditiis?</p><br>
+   
+</section>`
+  document.getElementById("m1").innerHTML=my
+  document.getElementById("m2").innerHTML=sy
+
+}
+
+
+
+
+function ma(){
+my=`  <section class="o1" id="t2">
+    <img src="" id="p2" alt="" onclick="abc()">
+    <section id="s1"></section>    
+  </section>
+  <section id="a12">
+    <div>
+      <h1 id="h2">Loading...<img src="3.png" id="k8" alt="" onclick="bnl()"></h1> 
+    </div>  
+    <br>
+    <div id="c11"></div>
+  </section>`
+ 
+  sy=`
+ #e2,#p3{
+  background-color: #14b429ff;}
+
+#p2{  width: 250px;
+      border: none;
+      margin: auto;
+      margin-top: 10px;
+      border-radius:50%;
+      cursor: pointer;
+      background-color: #8eb3a6;
+}
+.o1{
+  border: solid rgb(83, 68, 68) 1px;
+  width: 300px;
+  margin: auto;
+  padding: auto;
+  text-align: center;
+  background-color: rgba(0, 255, 255, 0.301);
+  border-radius: 15px;
+  margin-top: 2%;
+}
+
+
+  
+  
+  #p3{
+  width: 50px;}
+  #p6,#p4,#p5{  width: 35px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      background-color: #8eb3a6;
+  }.icon{
+  width: 50px;
+  background-color: #f1ecec85;
+  border-radius: 1%;
+}
+.d1{
+margin: 2%;
+padding: 10px;
+width: 40%;
+border-radius: 520px;
+max-width: 300px;
+min-width: 200px;
+}
+
+@media (max-width: 620px) {
+.d1 {
+    width: 80%;
+  }
+}
+
+#r6{
+  height: 200px;
+  width: 200px;
+  cursor: pointer;
+}
+#k8{
+  width:30px;
+  margin: auto;
+  margin-left: px;
+  padding: 0px;
+}
+#w1{
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  margin: 0px;
+  padding: 0px;
+}
+#h2{
+  margin: 10px;
+  padding-bottom:3px;
+  text-align: center;
+ background-color: #ebafde00;
+ border-radius: 5px;
+  font-family: 'Courier New', Courier, monospace;
+}
+#t12{
+  margin-top: 10px;
+  padding: 8px 16px;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: #1e693e;
+}
+#h2:hover {
+  transform: scale(1.08);  /* শুধু যেটাতে hover করবে সেটাই বড় হবে */
+}.d1{
+  font-size: x-large;
+  background-color: rgba(245, 239, 239, 0.473);
+  color: black;
+}
+
+`
+  document.getElementById("m1").innerHTML=my 
+  document.getElementById("m2").innerHTML=sy 
+  // এখন লোড করা হবে লোকালস্টোরেজ থেকে
+    const savedName = localStorage.getItem("savedName");
+    const savedImg = localStorage.getItem("profilePic");
+  
+    if (savedName) {
+      document.getElementById("h2").innerHTML = savedName + ` <button id="w1" onclick="bnl()">
+        <img src="https://cdn-icons-png.flaticon.com/128/2951/2951136.png" id="k8" alt="edit">
+      </button>`;
+    }
+  
+    if (savedImg) {
+      document.getElementById('p2').src = savedImg;
+    } else {
+      document.getElementById('p2').src = "https://cdn-icons-png.freepik.com/512/10797/10797438.png";
+    }
+  }
+
+
+
+
+
+function sa(){
+my=``
+sy=`  #p4{
+  width: 50px;}
+  #p6,#p3,#p5{  width: 35px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      background-color: #8eb3a6;
+  
+      
+}    #e3,#p4{
+  background-color: #14b429ff;}`
+  document.getElementById("m1").innerHTML=my
+  document.getElementById("m2").innerHTML=sy
+}
+
+
+
+
+function ta(){
+my=``
+sy=`  #p5{
+  width: 50px;}
+  #p6,#p4,#p3{  width: 35px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      background-color: #8eb3a6;
+      
+} #e4,#p5{
+  background-color: #14b429ff;}`
+  document.getElementById("m1").innerHTML=my
+  document.getElementById("m2").innerHTML=sy
+}
+
+
+
+
+
+// Profile Picture Change Code
+function triggerUpload() {
+  document.getElementById('fileInput').click();
+}
+
+function saveImg(event) {
+  const reader = new FileReader();
+  reader.onload = function() {
+    localStorage.setItem('profilePic', reader.result);
+    document.getElementById('p2').src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
+}
+
+window.onload = function() {
+  const savedImg = localStorage.getItem('profilePic');
+  if (savedImg) {
+    document.getElementById('p2').src = savedImg;
+  }
+}
+
+
+
+function abc(){document.getElementById("s1").innerHTML=
+  `<div class="img-container">
+  <img src="https://cdn-icons-png.freepik.com/512/8191/8191584.png" class="icon" id="editIcon" onclick="triggerUpload()">
+  <input type="file" id="fileInput" accept="image/*" onchange="saveImg(event)" hidden>
+</div>`
+}
+
+
+function bnl(){
+  document.getElementById("c11").innerHTML=`<input type="text" class="d1" id="l11" placeholder="fast name" onkeydown="if(event.key==='Enter'){addTask()}">
+    <input type="text" class="d1" id="l22" placeholder="last name" onkeydown="if(event.key==='Enter'){addTask()}">
+    <br><button id="t12" onclick="addname()">Change</button>`
+} 
+
+
+
+function addname() {
+  let l11 = document.getElementById("l11").value;
+  let l22 = document.getElementById("l22").value;
+  let nam = l11 + " " + l22;
+
+  // Display new name
+  document.getElementById("h2").innerHTML = nam + ` <button id="w1" onclick="bnl()">
+    <img src="https://cdn-icons-png.flaticon.com/128/2951/2951136.png" id="k8" alt="">
+  </button>`;
+
+  // Save to localStorage
+  localStorage.setItem("savedName", nam);
+
+  // Clear input
+  document.getElementById("c11").innerHTML = ``;
+}
